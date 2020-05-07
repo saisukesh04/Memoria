0ea6fd1a24a9e0b27229f1c95433e2d44c0f70ae
Sai Sukesh <sukesh.sai04@gmail.com>
2020-05-07 21:36:09 +0530

    Firebase signUp completed

---
+++ app/src/main/java/com/example/memoria/signup/RegisterActivity.java
@@ -0,0 +1,74 @@
+package com.example.memoria.signup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+
+import com.example.memoria.MainActivity;
+import com.example.memoria.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.regUsername) TextInputEditText regUsername;
+    @BindView(R.id.regPass) TextInputEditText regPass;
+    @BindView(R.id.regConfPass) TextInputEditText regConfPass;
+    @BindView(R.id.signUpBtn) Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                assert imm != null;
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+                String username = regUsername.getText().toString();
+                String pass = regPass.getText().toString();
+                String passConf = regConfPass.getText().toString();
+
+                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(passConf)){
+                    Snackbar.make(v, "Please fill all entries", Snackbar.LENGTH_LONG).show();
+                }else if (!pass.equals(passConf)){
+                    Snackbar.make(v, "Passwords do not match", Snackbar.LENGTH_LONG).show();
+                }else{
+                    mAuth.createUserWithEmailAndPassword(username, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.i("RegisterActivity: ", "createUserWithEmail : success");
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                            } else {
+                                Log.e("RegisterActivity", "createUserWithEmail : failure", task.getException());
+                                Snackbar.make(v, "Authentication failed. \n" + task.getException(), Snackbar.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/memoria/signup/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.memoria.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.memoria.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
